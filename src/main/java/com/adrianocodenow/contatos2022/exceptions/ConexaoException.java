3c4d92fb121071b3041aab1be1588c8e2b64fd1e
ElderBR <elderbarbosaromao@gmail.com>
2025-05-12 08:46:14 -0300

    Criado exception personalizada para o banco de dados

---
+++ src/main/java/com/adrianocodenow/contatos2022/exceptions/ConexaoException.java
@@ -0,0 +1,25 @@
+package com.adrianocodenow.contatos2022.exceptions;
+
+import com.adrianocodenow.contatos2022.utils.Msg;
+
+/**
+ *
+ * @author ElderBR
+ */
+public class ConexaoException extends RuntimeException {
+
+    public ConexaoException() {
+        Msg.ServerErro(getMessage(), this);
+    }
+
+    public ConexaoException(String message, Exception error) {
+        super(message, error);
+        Msg.ServerErro(message, error);
+    }
+
+    public ConexaoException(String message, String method, Exception error) {
+        super(message, error);
+        Msg.ServerErro(message, method, error);
+    }
+
+}
