2456ff55ec57ef63da0d35bbac1b149a063de764
ElderBR <elderbarbosaromao@gmail.com>
2025-05-13 10:02:24 -0300

    Colors: Criado cores personalizadas

---
+++ src/main/java/com/adrianocodenow/contatos2022/utils/Colors.java
@@ -0,0 +1,46 @@
+package com.adrianocodenow.contatos2022.utils;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author ElderBR
+ */
+public class Colors {
+
+    public static Color Blue() {
+        return new Color(0, 123, 255);
+    }
+
+    public static Color Green() {
+        return new Color(40, 167, 69);
+    }
+    
+    public static Color Dark_green(){
+        return new Color(0, 85, 0);
+    }
+
+    public static Color Red() {
+        return new Color(220, 53, 69);
+    }
+
+    public static Color Yellow() {
+        return new Color(255, 193, 7);
+    }
+
+    public static Color gray() {
+        return new Color(235, 237, 239);
+    }
+
+    public static Color tbLine() {
+        return gray();
+    }
+
+    public static Color select() {
+        return Yellow();
+    }
+    
+    public static Color BACKGROUND(){
+        return new Color(213, 216, 222);
+    }
+}
