3af83196c87ad10af8399a470f6a85f97b306729
ElderBR <elderbarbosaromao@gmail.com>
2025-05-13 10:55:41 -0300

    PhoneTypeCtrl: Criado a classe de controle para o tipo de telefone com o método de inserir

---
+++ src/main/java/com/adrianocodenow/contatos2022/controller/PhoneTypeCtrl.java
@@ -0,0 +1,65 @@
+package com.adrianocodenow.contatos2022.controller;
+
+import com.adrianocodenow.contatos2022.dao.PhoneTypeDao;
+import com.adrianocodenow.contatos2022.exceptions.PhoneException;
+import com.adrianocodenow.contatos2022.interfaces.IPhoneType;
+import com.adrianocodenow.contatos2022.model.PhoneType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.swing.DefaultListModel;
+import javax.swing.JTextField;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ *
+ * @author ElderBR
+ */
+public class PhoneTypeCtrl extends DefaultListModel<IPhoneType> {
+
+    private static PhoneTypeCtrl instance;
+    private PhoneTypeDao dao;
+    private IPhoneType phoneType;
+    private List<IPhoneType> listType = new ArrayList<>();
+
+    private PhoneTypeCtrl() {
+        dao = PhoneTypeDao.getInstance();
+        listType = dao.findAll();
+    }
+
+    public static PhoneTypeCtrl getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new PhoneTypeCtrl();
+        }
+        return instance;
+    }
+
+    public void save(@NotNull JTextField tfPhoneType) {
+        String type = tfPhoneType.getText().trim();
+        if (Objects.isNull(type) || type.isBlank()) {
+            throw new PhoneException("O nome do tipo de telefone inválido!");
+        }
+        if (type.length() < 3) {
+            throw new PhoneException("O nome do tipo de telefone precisa ser maior 2 letras!");
+        }
+        PhoneType phoneType = new PhoneType(type);
+        dao.insert(phoneType);
+        tfPhoneType.setText("");
+        tfPhoneType.requestFocus();
+        
+        // Busca a lista
+        listType = dao.findAll();        
+        fireContentsChanged(this, listType.size()-1, listType.size()-1);
+    }
+
+    @Override
+    public IPhoneType getElementAt(int index) {
+        phoneType = listType.get(index);
+        return phoneType;
+    }
+
+    @Override
+    public int getSize() {
+        return listType.size();
+    }
+}
