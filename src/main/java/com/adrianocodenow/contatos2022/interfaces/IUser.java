6d175778c2c2996a8997414ba114c334d18fe1b1
ElderBR <elderbarbosaromao@gmail.com>
2025-05-13 19:16:33 -0300

    IUser: Criado os métodos para retorna a data formatada como yyyy-MM-dd

--- src/main/java/com/adrianocodenow/contatos2022/interfaces/IUser.java
+++ src/main/java/com/adrianocodenow/contatos2022/interfaces/IUser.java
@@ -2,6 +2,7 @@ package com.adrianocodenow.contatos2022.interfaces;
 
 import com.adrianocodenow.contatos2022.exceptions.UserException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
@@ -9,11 +10,19 @@ import java.time.LocalDate;
  */
 public interface IUser extends IUserSimples{
     
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    
     IUser setDateCreation(LocalDate date);
     LocalDate getDateCreation();
+    default String toDateCreation(){
+        return getDateCreation().format(formatter);
+    }
     
     IUser setDateUpdated();
     LocalDate getDateUpdated();
+    default String toDateUpdated(){
+        return getDateUpdated().format(formatter);
+    }
     
     default <U extends IUser> U toUser(Class<U> clazz){
         try {
