5b67aa2a8173f1fb2daf694b41a65e5bc5c45835
ElderBR <elderbarbosaromao@gmail.com>
2025-05-14 08:24:39 -0300

    UserCtrl: Fazendo validação ao inserir novo usuário

--- src/main/java/com/adrianocodenow/contatos2022/controller/UserCtrl.java
+++ src/main/java/com/adrianocodenow/contatos2022/controller/UserCtrl.java
@@ -13,41 +13,45 @@ import javax.swing.JTextField;
  *
  * @author ElderBR
  */
-public class UserCtrl extends DefaultListModel<IUser>{
-    
+public class UserCtrl extends DefaultListModel<IUser> {
+
     private static UserCtrl instance;
     private UserDao userDao;
 
     private UserCtrl() {
         userDao = UserDao.getInstance();
     }
-    
-    public static UserCtrl getInstance(){
-        if(Objects.isNull(instance)){
+
+    public static UserCtrl getInstance() {
+        if (Objects.isNull(instance)) {
             instance = new UserCtrl();
         }
         return instance;
     }
-    
-    public void save(JTextField tfName, JTextField tfLastName){
-        if(Objects.isNull(tfName)){
+
+    public void save(JTextField tfName, JTextField tfLastName) {
+        if (Objects.isNull(tfName)) {
             throw new UserException("Campo nome obrigatório!");
         }
         String name = tfName.getText().trim();
-        if(name.isBlank() || name.length() < 3){
+        if (name.isBlank() || name.length() < 3) {
             throw new UserException("Campo nome obrigatório e não pode conter menos de 2 letras!");
         }
-        if(Objects.isNull(tfLastName)){
+        if (Objects.isNull(tfLastName)) {
             throw new UserException("Campo sobrenome obrigatório!");
         }
         String lastName = tfLastName.getText().trim();
-        if(lastName.isBlank() || lastName.length() < 3){
+        if (lastName.isBlank() || lastName.length() < 3) {
             throw new UserException("Campo sobrenome obrigatório e não pode conter menos de 2 letras!");
         }
         User user = new User();
         user.setNameUser(name);
-        user.setLastNameUser(lastName);        
+        user.setLastNameUser(lastName);
         user.setDateCreation(LocalDate.now());
+
+        if (!Objects.isNull(userDao.findByNameFull(user))) {
+            throw new UserException("Usuário já existe!");
+        }
         userDao.insert(user);
     }
 }
